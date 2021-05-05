6d29c9788de431b6c83b97c83d521ab3107bfac0
dumitru <dumitrudeleste@gmail.com>
2021-05-05 11:05:24 +0200

    Copia 2, poyecto funcional sin imagenes

--- app/src/main/java/com/example/androidapi_5/ConnectionHandler.java
+++ app/src/main/java/com/example/androidapi_5/ConnectionHandler.java
@@ -1,5 +1,6 @@
 package com.example.androidapi_5;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -25,119 +26,121 @@ public class ConnectionHandler {
 
     }
 
-    //-------------------------------------------------
+
+    //--------------------Meotodo para la conexion-----------------------------
     public HttpsURLConnection makeConnection() {
+
         HttpsURLConnection conn = null;
 
-                try {
-                    URL url = new URL("https://monedaiberica.org/dedalo/lib/dedalo/publication/server_api/v1/json/");
-                    conn = (HttpsURLConnection) url.openConnection();
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-                    conn.setRequestProperty("Accept", "application/json");
-                    conn.setDoOutput(true);
-                    conn.setDoInput(true);
+        try {
+            URL url = new URL("https://monedaiberica.org/dedalo/lib/dedalo/publication/server_api/v1/json/");
+            conn = (HttpsURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
 
-                    Log.d("HOLA", "HOLA");
+            Log.d("HOLA", "HOLA");
 
-                    JSONObject jsonParam = new JSONObject();
-                    jsonParam.put("dedalo_get", "records");
-                    jsonParam.put("table", "coins");
-                    jsonParam.put("code", "654asdiKrhdTetQksluoQaW2");
-                    jsonParam.put("db_name", "web_numisdata_mib");
-                    jsonParam.put("lang", "lg-eng");
-                    jsonParam.put("limit", 10);
+            JSONObject jsonParam = new JSONObject();
+            jsonParam.put("dedalo_get", "records");
+            jsonParam.put("table", "coins");
+            jsonParam.put("code", "654asdiKrhdTetQksluoQaW2");
+            jsonParam.put("db_name", "web_numisdata_mib");
+            jsonParam.put("lang", "lg-eng");
+            jsonParam.put("limit", 10);
 
-                    Log.d("HOLA", "HOLA");
+            Log.d("HOLA", "HOLA");
 
-                    Log.d("JSON", jsonParam.toString());
-                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
-                    os.writeBytes(jsonParam.toString());
+            Log.d("JSON", jsonParam.toString());
+            DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+            //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+            os.writeBytes(jsonParam.toString());
 
-                    os.flush();
-                    os.close();
+            os.flush();
+            os.close();
 
 
-                } catch (Exception ex) {
-                    Log.d("Exception 1 --> ", ex.toString());
-                }
-                
+        } catch (Exception ex) {
+            Log.d("Exception 1 --> ", ex.toString());
+        }
+
         return conn;
     }
 
-    //----------------------------------------------------------------
+    //-------------------------Metodo para obtener los datos---------------------------------------
     public ArrayList<String> getJsonData(HttpsURLConnection conn) {
+        try {
+            if (conn.getResponseCode() == 200) {
+
+
+                InputStream in = new BufferedInputStream(conn.getInputStream());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                StringBuilder sb = new StringBuilder();
+
+                String line;
                 try {
-                    if (conn.getResponseCode() == 200) {
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line).append('\n');
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                //-------------------
+                String jsonStr = sb.toString();
+
+                Log.d("RESPUESTA", "Response from url: " + jsonStr);
+                if (jsonStr != null) {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    Log.d("JSON", String.valueOf(jsonObj));
 
 
-                        InputStream in = new BufferedInputStream(conn.getInputStream());
-
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                        StringBuilder sb = new StringBuilder();
-
-                        String line;
-                        try {
-                            while ((line = reader.readLine()) != null) {
-                                sb.append(line).append('\n');
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } finally {
-                            try {
-                                in.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        //-------------------
-                        String jsonStr = sb.toString();
-
-                        Log.d("RESPUESTA", "Response from url: " + jsonStr);
-                        if(jsonStr != null){
-                            JSONObject jsonObj = new JSONObject(jsonStr);
-                            Log.d("JSON", String.valueOf(jsonObj));
+                    // Getting JSON Array node
+                    JSONArray contacts = jsonObj.getJSONArray("result");
 
 
-                            // Getting JSON Array node
-                            JSONArray contacts = jsonObj.getJSONArray("result");
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+
+                        String number = c.getString("number");
+                        String mint = c.getString("mint");
+                        String image_obverse = c.getString("image_obverse");
+                        String image_reverse = c.getString("image_reverse");
+                        String date_in = c.getString("date_in");
+                        String date_out = c.getString("date_out");
+                        String material = c.getString("material");
+                        String denomination = c.getString("denomination");
+
+                        lista_datos.add(number);
+                        lista_datos.add(mint);
+                        lista_datos.add(image_obverse);
+                        lista_datos.add(image_reverse);
+                        lista_datos.add(date_in);
+                        lista_datos.add(date_out);
+                        lista_datos.add(material);
+                        lista_datos.add(denomination);
 
 
-                            // looping through All Contacts
-                            for (int i = 0; i < contacts.length(); i++) {
-                                JSONObject c = contacts.getJSONObject(i);
-
-                                String number = c.getString("number");
-                                String mint = c.getString("mint");
-                                String image_obverse = c.getString("image_obverse");
-                                String image_reverse = c.getString("image_reverse");
-                                String date_in = c.getString("date_in");
-                                String date_out = c.getString("date_out");
-                                String material = c.getString("material");
-                                String denomination = c.getString("denomination");
-
-                                lista_datos.add(number);
-                                lista_datos.add(mint);
-                                lista_datos.add(image_obverse);
-                                lista_datos.add(image_reverse);
-                                lista_datos.add(date_in);
-                                lista_datos.add(date_out);
-                                lista_datos.add(material);
-                                lista_datos.add(denomination);
-
-
-                                Log.d("tabla", number + " " + mint + " " + image_obverse + " " + image_reverse + " " + date_in + " " +date_out + " " +material + " "+denomination);
-
-                            }
-                        }
+                        Log.d("tabla", number + " " + mint + " " + image_obverse + " " + image_reverse + " " + date_in + " " + date_out + " " + material + " " + denomination);
 
                     }
-
-
-                } catch (Exception ex) {
-                    Log.d("Exception 2--> ", ex.toString());
                 }
+
+            }
+
+
+        } catch (Exception ex) {
+            Log.d("Exception 2--> ", ex.toString());
+        }
         return lista_datos;
     }
 
--- app/src/main/java/com/example/androidapi_5/MainActivity.java
+++ app/src/main/java/com/example/androidapi_5/MainActivity.java
@@ -2,23 +2,65 @@ package com.example.androidapi_5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
     ConnectionHandler connectionHandler = new ConnectionHandler();
+    GetData data;
 
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       HttpsURLConnection conn = connectionHandler.makeConnection();
-       connectionHandler.getJsonData(conn);
+        lv = findViewById(R.id.listView1);
+
+        new GetData().execute();
 
 
+    }
+
+
+    private class GetData extends AsyncTask<Void,Void,Void> {
+        ConnectionHandler connectionHandler = new ConnectionHandler();
+        ArrayList<String> lista_datos = new ArrayList<String>();
+
+
+        public GetData() {
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Toast.makeText(MainActivity.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpsURLConnection conn = connectionHandler.makeConnection();
+            lista_datos = connectionHandler.getJsonData(conn);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, lista_datos);
+            lv.setAdapter(adapter);
+
+
+        }
     }
 }
